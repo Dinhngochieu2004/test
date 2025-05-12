e390b89c597ed93767e5c3dee49c4c234a15a8d3
Đinh Ngọc Hiếu <Dinhngochieud@gmail.com>
2025-05-12 11:56:48 +0700

    Update category.ts

--- src/apps/controllers/category.ts
+++ src/apps/controllers/category.ts
@@ -2,7 +2,7 @@ import express from "express";
 import CategoryModel from "../models/category";
 
 class CategoryController {
-    async index(request: express.Request, res: express.Response){
+    async index(req: express.Request, res: express.Response){
         try {
             const categories = await CategoryModel.find();
             res.status(200).json(categories);
